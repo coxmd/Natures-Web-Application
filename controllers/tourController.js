89b4d1342ebea4e583226824483a6073b8b16c81
coxMD <coxmusyoki@gmail.com>
2022-10-01 13:16:10 +0300

    Implemented delete tour functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,4 @@
+const { findByIdAndDelete } = require('./../models/tourModel');
 const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
@@ -76,9 +77,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
