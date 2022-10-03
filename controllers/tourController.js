2a9dab6748612162212ddc547dafd7d586740b1c
coxMD <coxmusyoki@gmail.com>
2022-10-03 10:31:20 +0300

    implemented Advanced filtering on API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,21 +3,28 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // EXCECUTE QUERY
+    const tours = await query;
     // const query =  Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
 
-    // EXCECUTE QUERY
-    const tours = await query;
     // SEND RESPONSE
     res.status(200).json({
       status: 'success',
