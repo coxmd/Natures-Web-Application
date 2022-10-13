a5b466f62ad00657081968c1be51d9586284c86b
coxMD <coxmusyoki@gmail.com>
2022-10-13 12:47:50 +0300

    Implemented password reset functionality

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -41,7 +42,9 @@ const userSchema = new mongoose.Schema({
   },
   passwordChangedAt: {
     type: Date
-  }
+  },
+  passwordResetToken: String,
+  passwordResetExpires: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -53,6 +56,13 @@ userSchema.pre('save', async function(next) {
 
   // Delete password confirm field
   this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
   next();
 });
 
@@ -76,6 +86,19 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
   }
   // flase means not changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
