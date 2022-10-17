8c9b8f9d8d1aff56243c2d861eeff3bebaeb1360
coxMD <coxmusyoki@gmail.com>
2022-10-17 12:13:13 +0300

    implemented geospatial data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -19,9 +19,7 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 // READ JSON FILE
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // IMPORT DATA INTO DATABASE
 const importData = async () => {
