146bc4e141e1340e1a9a42586161c2c91120b404
coxMD <coxmusyoki@gmail.com>
2022-10-19 21:15:57 +0300

    Implemented factory function for reading documents

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,18 +12,6 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users
-    }
-  });
-});
-
 exports.updateMe = catchAsync(async (req, res, next) => {
   //1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -61,18 +49,16 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!'
-  });
-};
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not yet defined!'
+    message: 'This route is not defined! Please use /signup instead'
   });
 };
+
+exports.getUser = factory.getOne(User);
+exports.getAllUsers = factory.getAll(User);
+
 // Do not update passwords with this
 exports.updateUser = factory.updateOne(User);
 
