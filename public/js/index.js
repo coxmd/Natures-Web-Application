687b8bdbfb868435ee558b5b539c6850d261fe5c
coxMD <coxmusyoki@gmail.com>
2022-10-29 12:38:45 +0300

    Implemented logout authentication in the frontend

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,10 @@
 /* eslint-disable */
 import '@babel/polyfill';
-import { login } from './login';
+import { login, logout } from './login';
 
 // DOM ELEMENTS
 const loginForm = document.querySelector('.form');
+const logoutBtn = document.querySelector('.nav__el--logout');
 
 if (loginForm) {
   loginForm.addEventListener('submit', e => {
@@ -12,4 +13,8 @@ if (loginForm) {
     const password = document.getElementById('password').value;
     login(email, password);
   });
+}
+
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', logout);
 }
